c1a449b66df57fdae9d1a7de2e266374a9ddabb7
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2024-12-01 16:17:37 -0600

    Day1 of Advent of Code

---
+++ Day1/Day1-Part2.py
@@ -0,0 +1,28 @@
+def solve(file):
+    #Define lists to hold numbers
+    result = 0
+    left = []
+    right = []
+
+    #Prep data from file and create lists
+    f = open(file).readlines()
+    y = [x.replace('\n','') for x in f]
+
+    for s in y:
+        nums = s.split('   ')
+        left.append(int(nums[0]))
+        right.append(int(nums[1]))
+
+
+    #Sort the lists so they are in order
+    left.sort()
+    right.sort()
+
+    #Calculate the result
+    for i in range(0,len(left)):
+        result += left[i]* right.count(left[i])
+
+    print(result)
+
+if __name__ == '__main__':
+    solve('input.txt')
